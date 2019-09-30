e2097d8e2a6ac6a6036aa4357b2b88e57c7fec5b
leeli <leeli@leelis-iMac-2.local>
2019-09-30 18:40:36 +0800

    update entry

--- lib/entry.rb
+++ lib/entry.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 class Entry
   attr_accessor :entry, :form_fields
-  attr_reader :result, :xml_factory, :form_keys, :show_fileds
+  attr_reader :result, :xml_factory, :form_keys, :show_fileds, :default_reject_fields
 
   # hd=hash_data
   # params:
@@ -21,6 +21,8 @@ class Entry
     # xml_factory.form_keys: xml的生成较久，需要0.03s,所以需要把数据存出来
     @form_fields = xml_factory.form_fields
     @form_keys = xml_factory.form_keys
+
+    @default_reject_fields = ['发票号', '邮寄状态','提交人', '微信头像', '微信OpenID', '微信昵称', '微信性别', '微信国家', '微信省市', '修改时间', 'IP', "填写设备", "操作系统", "浏览器"]
   end
 
   def set_show_values
@@ -31,16 +33,6 @@ class Entry
     else
       form_fields
     end
-  end
-
-  def filter_show_fileds
-    buf = []
-    form_fields.each do |hash|
-      v = hash.values.first
-      label = v["label"]
-      buf << hash if show_fileds.include?(label)
-    end
-    buf
   end
 
   def set_form_field_values
@@ -54,8 +46,15 @@ class Entry
     form_fields
   end
 
+  def filter_show_fileds
+    form_fields.select do |hash|
+      v = hash.values.first
+      label = v["label"]
+      show_fileds.include?(label)
+    end
+  end
+
   def remove_default_reject_fields
-    default_reject_fields = ['发票号', '邮寄状态','提交人', '微信头像', '微信OpenID', '微信昵称', '微信性别', '微信国家', '微信省市', '修改时间', 'IP', "填写设备", "操作系统", "浏览器"]
     form_fields.delete_if do |hash|
       v = hash.values.first
       label = v["label"]
