ecc854b8d8ba03f0bdb6834ad359714afa78debe
leeli <oolzpishere@126.com>
2019-11-18 01:12:19 +0800

    update jinshuju to block method

--- lib/insert_records/jinshuju.rb
+++ lib/insert_records/jinshuju.rb
@@ -3,31 +3,49 @@ module InsertRecords
     # InsertRecords::Jinshuju.new("0llDiH").batch_query
 
     def batch_query
-      response, response_body_hash = request_loop
+      response, response_body_hash = request_loop_block {|response| create_records(response) }
       while response_body_hash["next"]
-        response, response_body_hash = request_loop(response_body_hash["next"])
+        response, response_body_hash = request_loop_block(response_body_hash["next"]) {|response| create_records(response) }
       end
 
     end
 
-    def request_loop(next_num = nil)
+    def batch_print
+      response, response_body_hash = request_loop_block {|response| print_all_records(response) }
+      while response_body_hash["next"]
+        response, response_body_hash = request_loop_block(response_body_hash["next"]) {|response| print_all_records(response) }
+      end
+    end
+
+    def request_loop_block(next_num = nil, &block)
       request = request(next_num)
       response = request.response
       raise "request form data fail. #{response.body}" unless response.success?
+      # response is array of multiple datas.
       response_body_hash = JSON.parse(response.body)
 
-      batch_create(response_body_hash["data"])
+      yield(response_body_hash["data"])
 
       return response, response_body_hash
     end
 
-    def batch_create(data_array)
+    def create_records(data_array)
       records = []
       @form_obj = Form.find_or_create_by(form: form)
       data_array.each do |record|
         records << Match.new( match_params(record) )
       end
       Match.import records
+    end
+
+    def print_all_records(data_array)
+      records = []
+      @form_obj = Form.find_or_create_by(form: form)
+      data_array.each do |record|
+        puts record
+        records << Match.new( match_params(record) )
+      end
+      # puts records
     end
 
     def match_params(record)
